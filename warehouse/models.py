92facc6118d8e613e5a2d8cf2ee308837ed525b7
raytr <giaotran2001lk@gmail.com>
2025-05-02 11:39:42 +0700

    Update

--- warehouse/models.py
+++ warehouse/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 class Warehouse(models.Model):
     wh_code = models.CharField(max_length=20, primary_key=True)
     wh_name = models.CharField(max_length=100)
+    wh_bg = models.CharField(max_length=100, blank=True, null=True)
+    wh_comment = models.CharField(max_length=100, blank=True, null=True)
     create_at = timezone.now(),
     create_by = models.ForeignKey(
         User,
